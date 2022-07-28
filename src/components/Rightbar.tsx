fed0c16496228c4b71c212264cf425b96799d901
Szymon Miękus <miekusoft@gmail.com>
2022-07-28 22:03:00 +0200

    Rightbar in progress

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,14 +1,62 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+];
 
 const Rightbar = () => {
   return (
-    <Box
-      bgcolor="yellow"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>
+          Online friends
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100}>
+          Latest photos
+        </Typography>
+        <ImageList
+          sx={{ width: 500, height: 450 }}
+          cols={3}
+          rowHeight={100}
+          gap={5}
+        >
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
     </Box>
   );
 };
