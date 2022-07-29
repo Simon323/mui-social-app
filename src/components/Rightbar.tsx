a95ad747e5bd5223fbdc2e193b8e3679716194de
Szymon Miękus <miekusoft@gmail.com>
2022-07-29 22:47:08 +0200

    Rightbar complete

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -2,10 +2,16 @@ import {
   Avatar,
   AvatarGroup,
   Box,
+  Divider,
   ImageList,
   ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Typography,
 } from "@mui/material";
+import React from "react";
 
 const itemData = [
   {
@@ -24,7 +30,7 @@ const itemData = [
 
 const Rightbar = () => {
   return (
-    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+    <Box flex={1.5} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed" width={300}>
         <Typography variant="h6" fontWeight={100}>
           Online friends
@@ -36,15 +42,10 @@ const Rightbar = () => {
           <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
           <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
         </AvatarGroup>
-        <Typography variant="h6" fontWeight={100}>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
           Latest photos
         </Typography>
-        <ImageList
-          sx={{ width: 500, height: 450 }}
-          cols={3}
-          rowHeight={100}
-          gap={5}
-        >
+        <ImageList cols={3} rowHeight={100} gap={5}>
           {itemData.map((item) => (
             <ImageListItem key={item.img}>
               <img
@@ -56,6 +57,78 @@ const Rightbar = () => {
             </ImageListItem>
           ))}
         </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
