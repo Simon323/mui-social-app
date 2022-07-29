ff48fbcf52c7d5319e5a254e3c11f78f9260e547
Szymon Miękus <miekusoft@gmail.com>
2022-07-29 22:47:27 +0200

    Add button & modal completed

---
+++ src/components/Add.tsx
@@ -0,0 +1,100 @@
+import {
+  Box,
+  Fab,
+  Modal,
+  Tooltip,
+  styled,
+  Typography,
+  Avatar,
+  TextField,
+  Stack,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import {
+  Add as AddIcon,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+import { useState } from "react";
+import avatar from "assets/1.jpg";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
+
+const Add = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Tooltip
+        onClick={() => setOpen(true)}
+        title="Add"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50%)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              alt="Remy Sharp"
+              src={avatar}
+            />
+            <Typography fontWeight={500}>Jay Garrick</Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="multiline"
+            multiline
+            rows={4}
+            defaultValue="Post something"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup variant="contained" fullWidth>
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
+
+export default Add;
